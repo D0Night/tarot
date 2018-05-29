11d099f034762fe08bff827a36a09129a65702a7
Keng3326 <37768769+Keng3326@users.noreply.github.com>
2018-05-29 22:53:05 +0800

    Update check.php

--- check.php
+++ check.php
@@ -2,7 +2,7 @@
 session_start();
 
 if($_SESSION["login"]==true){
-	header("Location:#");
+	header("Location:index.html");
 }else{
 	echo "請先登入，將於3秒後移至登入網頁";
 	header("Refresh:3; url='login.html'");
