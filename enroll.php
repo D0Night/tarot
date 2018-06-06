8201a75440c5fae1f97c5dbf963aa45300260850
Keng3326 <37768769+Keng3326@users.noreply.github.com>
2018-06-06 18:38:16 +0800

    Update enroll.php

--- enroll.php
+++ enroll.php
@@ -1,7 +1,7 @@
 <?php
 $newname=$_POST['name'];
 $newpass=$_POST['password'];
-$newgender=$_POST['gender[]'];
+$newgender=$_POST['gender'];
 $newage=$_POST['age'];
 
 $link=mysqli_connect(
@@ -11,20 +11,24 @@ $link=mysqli_connect(
 	'user');
 mysqli_select_db($link,'user');
 
-$sql="SELECT * FROM member";
+$sql="SELECT * FROM user";
 $result=mysqli_query($link,$sql);
-$sql2="INSERT INTO member(name,password,gender,age) VALUES('$newname','$newpass','$newgender','$newage')";
 
+$check=0;
 if ($newname!=null && $newpass!=null && $newgender!=null && $newage!=null){
     while ($row=mysqli_fetch_assoc($result)){
-        if ($newname != $row['name'])
-            $insert=mysqli_query($link,$sql2);
-        else
-            echo "此帳號已有人使用，請重新輸入"
+        if ($newname == $row['name'])
+            $check=1;
     }
 }
 else
-    echo "欄位不可為空值，請重新輸入"
+    echo "欄位不可為空值，請重新輸入";
+
+$sql2="INSERT INTO member(name,password,gender,age) VALUES('$newname','$newpass','$newgender','$newage')";
+if($check==0)
+    $insert=mysqli_query($link,$sql2);
+else
+    echo "此帳號已有人使用，請重新輸入";
 
 
 ?>
