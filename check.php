7f75b3e930450d1074c8bba026597ff1bc3facec
Keng3326 <37768769+Keng3326@users.noreply.github.com>
2018-06-08 16:25:13 +0800

    Update check.php

--- check.php
+++ check.php
@@ -2,7 +2,7 @@
 session_start();
 
 if($_SESSION["login"]==true){
-	header("Location:index.html");
+	header("Location:game.html");
 }else{
 	echo "請先登入，將於3秒後移至登入網頁";
 	header("Refresh:3; url='login.html'");
