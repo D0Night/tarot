bd78cf3f84dd56d880f031e3c4a3aa8dfa4b73e4
Keng3326 <37768769+Keng3326@users.noreply.github.com>
2018-06-12 00:55:39 +0800

    Update enroll.php

--- enroll.php
+++ enroll.php
@@ -4,14 +4,14 @@ $newpass=$_POST['password'];
 $newgender=$_POST['gender'];
 $newage=$_POST['age'];
 
-$link=mysqli_connect(
-	'localhost',
-	'root',
-	'',
-	'user');
-mysqli_select_db($link,'user');
+$link=@mysqli_connect(
+    '140.127.218.154',
+    'root',
+    'tarot9605',
+    'tarot');
+mysqli_select_db($link,'tarot');
 
-$sql="SELECT * FROM user";
+$sql="SELECT * FROM member";
 $result=mysqli_query($link,$sql);
 
 $check=0;
@@ -25,10 +25,11 @@ else
     echo "欄位不可為空值，請重新輸入";
 
 $sql2="INSERT INTO member(name,password,gender,age) VALUES('$newname','$newpass','$newgender','$newage')";
-if($check==0)
+if($check==0){
     $insert=mysqli_query($link,$sql2);
-else
+    header("Location:login.html");
+}else
     echo "此帳號已有人使用，請重新輸入";
 
-
+mysqli_close($link);
 ?>
