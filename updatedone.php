2708450727764c2238b13539a3bd74d564d05e59
blossom0918 <39674227+blossom0918@users.noreply.github.com>
2018-06-14 22:33:39 +0800

    Update and rename updatedown.php to updatedone.php

---
+++ updatedone.php
@@ -0,0 +1,33 @@
+<?php
+
+	session_start();
+	$userID=$_COOKIE["ID"];
+
+	$userPASS=$_POST["password"];
+	$usergender=$_POST['gender'];
+	$userage=$_POST['age'];
+
+	$link=@mysqli_connect(
+	   '140.127.218.154',
+	    'root',
+	    'tarot9605',
+	    'tarot');
+	
+	mysqli_select_db($link,'tarot');
+
+	$sql="SELECT * FROM member WHERE name='$userID'";
+	$result=mysqli_query($link,$sql);
+
+	$sql2="UPDATE member SET password='$userPASS', gender='$usergender', age='$userage' WHERE name='$userID'";
+	$update=mysqli_query($link,$sql2);
+
+	$row=mysqli_fetch_assoc($result)
+	echo "帳號：".$row['name']."<br>";
+	echo "密碼：".$row['password']."<br>";
+	echo "性別：".$row['gender']."<br>";
+	echo "年齡：".$row['age']."<br>";
+	echo "a href='update.php'>再次編輯</a>"."<br>";	
+	
+
+	mysqli_close($link);
+?>
